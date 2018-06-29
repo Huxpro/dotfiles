9cfd46161a84f851a91b371e913d1fde6d1471c5
Xuan Huang <huxpro@gmail.com>
2018-06-29 00:38:23 -0700

    update linkall.sh

--- linkall.sh
+++ linkall.sh
@@ -7,7 +7,7 @@ ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/zsh/.zshenv ~/.zshenv
 ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/zsh/oh-my-zsh/custom/incr/ ~/.oh-my-zsh/plugins/incr
+ln -sf ~/dotfiles/zsh/oh-my-zsh/themes/refined-lambda.zsh-theme ~/.oh-my-zsh/themes/refined-lambda.zsh-theme
 
 # vim
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
