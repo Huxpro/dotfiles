9ed82e11deacc23e5637ab0c3d2f8beb1bea3d4d
Xuan Huang <huxpro@gmail.com>
2020-04-27 22:23:19 -0700

    * linkall.sh will detect nvim

--- linkall.sh
+++ linkall.sh
@@ -15,10 +15,14 @@ ln -sf "$cwd/zsh/oh-my-zsh/themes/refined-lambda.zsh-theme" ~/.oh-my-zsh/themes/
 ln -sf "$cwd/vim/.vimrc" ~/.vimrc
 ln -sf "$cwd/vim/.vimrc" ~/.ideavimrc
 
-# neovim
-ln -sf "$cwd/nvim/init.vim" ~/.config/nvim/init.vim
-ln -sf "$cwd/nvim/coc-settings.json" ~/.config/nvim/coc-settings.json
-ln -sf "$cwd/nvim/coc-settings.json" ~/.vim/coc-settings.json         # fallback to vim
+# neovim and coc
+if [ -d "~/.config/nvim" ]; then
+  ln -sf "$cwd/nvim/init.vim" ~/.config/nvim/init.vim
+  ln -sf "$cwd/nvim/coc-settings.json" ~/.config/nvim/coc-settings.json
+else
+  # fallback to vim
+  ln -sf "$cwd/nvim/coc-settings.json" ~/.vim/coc-settings.json
+fi
 
 # vscode
 ln -sf "$cwd/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
